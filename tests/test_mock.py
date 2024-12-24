4dbd36df76b0b6936be6de36c0c0b9f6699905c0
Ezekiel M. Alene <101397453+EzekielMisgae@users.noreply.github.com>
2024-12-24 09:59:50 +0300

    Create unittest in mock

---
+++ tests/test_mock.py
@@ -0,0 +1,8 @@
+from unittest.mock import MagicMock
+
+def external_service():
+    return "real data"
+
+def test_mocking():
+    mock_service = MagicMock(return_value="mock data")
+    assert mock_service() == "mock data"
